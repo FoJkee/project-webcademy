0fe5b44040ede9070364d5b0c26e0f6d5fb39bcc
FoJkee <romanovsky0815@gmail.com>
2023-05-03 18:14:48 +0300

    header

---
+++ project/src/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1 className='header-title'>Юрий Ключевский</h1>
+            <p className='header-subtitle'>
+                фрилансер, веб-разработчик,создание сайтов
+            </p>
+        </div>
+    );
+};
+
+export default Header;
