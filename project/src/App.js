9c2e7a732e5e3aefe2a87856632d379e4a109522
FoJkee <romanovsky0815@gmail.com>
2023-05-05 19:14:31 +0300

    end footer

--- project/src/App.js
+++ project/src/App.js
@@ -6,8 +6,8 @@ import Footer from './Footer/Footer';
 function App() {
     return (
         <>
-            {/* <Header /> */}
-            {/* <Main /> */}
+            <Header />
+            <Main />
             <Footer />
         </>
     );
--- project/src/Footer/Footer.jsx
+++ project/src/Footer/Footer.jsx
@@ -1,18 +1,49 @@
 import './Footer.css';
 import Button from './button/Button';
+import facebook from './img/facebook.svg';
+import github from './img/github.svg';
+import instagram from './img/instagram.svg';
+import linkedin from './img/linkedin.svg';
+import vk from './img/vk.svg';
 
 const Footer = () => {
     return (
         <div className='footer'>
             <div className='container'>
-                <div className='footer-row'>
+                <div className='footer__row'>
                     <div className='footer__copirigth'>
-                        © Юрий Ключевский HTML верстка и разработка сайтов
+                        <div className='footer__copirigth_name'>
+                            © Юрий Ключевский
+                        </div>
+                        <p>HTML верстка и разработка сайтов</p>
                     </div>
-                    <div className='footer__profile'>
-                        Мои профили в социальных сетях:
+                    <div className='footer__profile '>
+                        <p>Мои профили в социальных сетях:</p>
+                        <div className='footer__icons'>
+                            <a href='#'>
+                                <img src={instagram} alt='instagram' />
+                            </a>
+                            <a href='#'>
+                                <img src={facebook} alt='facebook' />
+                            </a>
+                            <a href='#'>
+                                <img src={vk} alt='vk' />
+                            </a>
+                            <a href='#'>
+                                <img src={linkedin} alt='linkedin' />
+                            </a>
+                            <a href='#'>
+                                <img src={github} alt='github' />
+                            </a>
+                        </div>
                     </div>
-                    <Button />
+                    <div className='footer_click footer__col'>
+                        <Button />
+                        <p>
+                            Напишите мне, чтобы заказать или узнать стоимость
+                            верстки вашего проекта
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
